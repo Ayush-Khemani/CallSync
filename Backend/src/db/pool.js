4ae88fdd0132225254320e6bbb3ccd7bee4851cb
Ayush Kumar <152976252+Ayush-Khemani@users.noreply.github.com>
2026-08-14 05:21:05 +0200

    Create Postgres pool lazily

--- Backend/src/db/pool.js
+++ Backend/src/db/pool.js
@@ -1,9 +1,25 @@
 const { Pool } = require('pg');
 const config = require('../config/env');
 
-const pool = new Pool({
-  connectionString: config.databaseUrl,
-  ssl: config.nodeEnv === 'production' ? { rejectUnauthorized: false } : false,
-});
+let pool;
 
-module.exports = pool;
+function getPool() {
+  if (!config.databaseUrl) {
+    throw new Error('DATABASE_URL is required for database operations');
+  }
+
+  if (!pool) {
+    pool = new Pool({
+      connectionString: config.databaseUrl,
+      ssl: config.nodeEnv === 'production' ? { rejectUnauthorized: false } : false,
+    });
+  }
+
+  return pool;
+}
+
+module.exports = {
+  query: (...args) => getPool().query(...args),
+  connect: (...args) => getPool().connect(...args),
+  end: (...args) => (pool ? pool.end(...args) : Promise.resolve()),
+};
