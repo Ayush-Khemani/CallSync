a9c13ac50fa6f18059c4392ce5050b0372dbf211
Ayush Kumar <152976252+Ayush-Khemani@users.noreply.github.com>
2026-08-18 15:35:44 +0200

    Route the root page to the new CallSync landing experience

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LandingPageV2 from './LandingPageV2';
 import reportWebVitals from './reportWebVitals';
+
+const NAVIGATION_EVENT = 'callsync:navigation';
+
+if (!window.__callsyncHistoryPatched) {
+  ['pushState', 'replaceState'].forEach((method) => {
+    const original = window.history[method];
+    window.history[method] = function patchedHistoryMethod(...args) {
+      const result = original.apply(this, args);
+      window.dispatchEvent(new Event(NAVIGATION_EVENT));
+      return result;
+    };
+  });
+  window.__callsyncHistoryPatched = true;
+}
+
+function RootApp() {
+  const [pathname, setPathname] = React.useState(window.location.pathname);
+
+  React.useEffect(() => {
+    const syncPath = () => setPathname(window.location.pathname);
+    window.addEventListener('popstate', syncPath);
+    window.addEventListener(NAVIGATION_EVENT, syncPath);
+    return () => {
+      window.removeEventListener('popstate', syncPath);
+      window.removeEventListener(NAVIGATION_EVENT, syncPath);
+    };
+  }, []);
+
+  return pathname === '/' ? <LandingPageV2 /> : <App />;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RootApp />
   </React.StrictMode>
 );
 
